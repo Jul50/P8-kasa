798ed1114b62bef306675e43e3bad4bd24bf6d81
Jules N <jules.nozet@outlook.fr>
2024-01-18 17:54:46 +0100

    Footer

--- src/compo/Footer.jsx
+++ src/compo/Footer.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./Footer.css";
 
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="Footer">
+      <div className="Footer__logo">
+        <img src="footerlogo.jpg" alt="logo du footer"></img>
+      </div>
+      <div className="Footer__txt">© 2020 Kasa. All rights reserved</div>
+    </div>
+  );
 }
 
 export default Footer;
